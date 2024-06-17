1ca09bc1799323982261db87088493273540deee
Ravipawar <ravikumar.pawar@helpshift.com>
2024-06-17 13:53:42 +0530

    removed secrets

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,9 +7,9 @@ class Constants {
   static const helpShiftDomain = 'ravi-demo.helpshift.com';
   static helpShiftApiKey() {
     if (Platform.isAndroid) {
-      return '166b4bd08f4a62fbb91f6dc965d81bf1';
+      return '16f1';
     } else if (Platform.isIOS) {
-      return "166b4bd08f4a62fbb91f6dc965d81bf1";
+      return "16f1";
     } else {
       return "";
     }
@@ -17,9 +17,9 @@ class Constants {
 
   static helpShiftAppId() {
     if (Platform.isAndroid) {
-      return 'ravi-demo_platform_20220331005029798-1e2a4b59980151a';
+      return 'ravi-demo_pl51a';
     } else if (Platform.isIOS) {
-      return "ravi-demo_platform_20220331005029716-39b80be81e0cfb1";
+      return "ravi-demo_plfb1";
     } else {
       return "";
     }
